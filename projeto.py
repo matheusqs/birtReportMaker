6cc4138607d0118f362eb3f967fa71f15811c5f3
matheus <matheus.quintao.santiago@gmail.com>
2019-07-02 23:34:59 -0300

    Implementei a parte que cria data Source

--- projeto.py
+++ projeto.py
@@ -3,11 +3,11 @@ import xml.etree.cElementTree as ET
 def createStandardReport():
     report = ET.Element("report", xmlns = "http://www.eclipse.org/birt/2005/design", version = "3.2.23", id = "1")
     createStandardReportPropertyReport(report)
+    createDataSource(report)
     createStandardReportStyles(report)
     createStandardReportPageSetup(report)
-    tree = ET.ElementTree(report)
-    tree.write(getReportName(), xml_declaration = True, encoding = "UTF-8", method = "xml")
-    
+    return report
+
 def createStandardReportPropertyReport(report):
     ET.SubElement(report, "property", name = "createdBy").text = "Eclipse BIRT Designer Version <4.4.0.v201405191524 Build &lt;4.4.0.v20140606-1451>"
     ET.SubElement(report, "property", name = "units").text = "in"
@@ -92,9 +92,39 @@ def getReportName():
     reportName = ''.join(reportName)
     return reportName
 
-def main():
-    createStandardReport()
+def createDataSource(report):
+    dataSource = ET.SubElement(report, "data-sources")
+    createDataSourceOdaDataSource(dataSource)
 
+def createDataSourceOdaDataSource(dataSource):
+    odaDataSource = ET.SubElement(dataSource, "oda-data-source", extensionID = "org.eclipse.birt.report.data.oda.jdbc", name = "Data Source", id = "7")
+    createDataSourceOdaDataSourceProperties(odaDataSource)
+
+def createDataSourceOdaDataSourceProperties(odaDataSource):
+    ET.SubElement(odaDataSource, "text-property", name = "displayName")
+    listProperty = ET.SubElement(odaDataSource, "list-property", name = "privateDriverProperties")
+    createDataSourceOdaDataSourcePropertiesListProperty(listProperty)
+    ET.SubElement(odaDataSource, "property", name = "odaDriverClass").text = "oracle.jdbc.driver.OracleDriver"
+    ET.SubElement(odaDataSource, "property", name = "odaURL").text = "jdbc:oracle:thin:@192.168.122.5:1521/PDBORCL"
+    ET.SubElement(odaDataSource, "property", name = "odaUser").text = "vivo_sabor"
+    ET.SubElement(odaDataSource, "encrypted-property", name = "odaPassword", encryptionID = "base64").text = "dGVrbmlzYQ=="
+
+def createDataSourceOdaDataSourcePropertiesListProperty(listProperty):
+    exProperty = ET.SubElement(listProperty, "ex-property")
+    ET.SubElement(exProperty, "name").text = "metadataBidiFormatStr"
+    ET.SubElement(exProperty, "value").text = "ILYNN"
+    exProperty = ET.SubElement(listProperty, "ex-property")
+    ET.SubElement(exProperty, "name").text = "disabledMetadataBidiFormatStr"
+    exProperty = ET.SubElement(listProperty, "ex-property")
+    ET.SubElement(exProperty, "name").text = "contentBidiFormatStr"
+    ET.SubElement(exProperty, "value").text = "ILYNN"
+    exProperty = ET.SubElement(listProperty, "ex-property")
+    ET.SubElement(exProperty, "name").text = "disabledContentBidiFormatStr"
+
+def main():
+    report = createStandardReport()
+    reportTree = ET.ElementTree(report)
+    reportTree.write(getReportName(), xml_declaration = True, encoding = "UTF-8", method = "xml")
 
 if __name__ == "__main__":
     main()
